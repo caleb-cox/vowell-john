1f637d94bb6b60aac18c5f1a81cf9dcab6c622a0
caleb-cox <cacox25@gmail.com>
2024-04-02 16:32:32 -0400

    Build out ReadView

--- src/components/ReadView/ReadView.jsx
+++ src/components/ReadView/ReadView.jsx
@@ -1,9 +1,18 @@
+import reactStringReplace from "react-string-replace";
+import { useAppContext } from "@/components/App";
 import "./ReadView.css";
 
 const ReadView = () => {
+  const { pages, currentPage } = useAppContext();
+
   return (
     <div className="ReadView">
-      <h1>Read View</h1>
+      <h1>{currentPage}</h1>
+      <pre>
+        {reactStringReplace(pages[currentPage], /\[\[(.*?)]]/g, (match) => (
+          <span className="clickable">{match}</span>
+        ))}
+      </pre>
     </div>
   );
 };
