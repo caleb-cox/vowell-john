01ef19c0604b2baaa770f152e7178b08dd22dc27
caleb-cox <cacox25@gmail.com>
2024-04-02 12:53:13 -0400

    Scaffold app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import eyeLogo from "/eye.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src={eyeLogo} style={{ width: "20rem" }} />
+    </>
+  );
+}
+
+export default App;
