0d3e87fa2b6d11f9420a5d625df5adb0e4a4271b
caleb-cox <cacox25@gmail.com>
2024-04-02 15:38:34 -0400

    Add pages state management

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,16 @@ import { createContext, useState, useEffect, useContext } from "react";
 import ModeSelector from "@/components/ModeSelector";
 import "./App.css";
 
+const localStorageIsAvailable = (() => {
+  try {
+    localStorage.setItem("__test", "__test");
+    localStorage.removeItem("__test");
+    return true;
+  } catch {
+    return false;
+  }
+})();
+
 const AppContext = createContext();
 
 const App = () => {
@@ -11,9 +21,30 @@ const App = () => {
     document.body.classList.value = `${mode}-mode`;
   }, [mode]);
 
+  const [pages, setPages] = useState({
+    "Hello world": "This is the first page, isn't it neat?",
+    "What else?": "This is another page. We have so many beautiful pages.",
+    "A THIRD page!": "Lorem ipsum blah blah butts",
+  });
+
+  const [currentPage, setCurrentPage] = useState(
+    (localStorageIsAvailable && localStorage.getItem("currentPage")) ||
+      Object.keys(pages)[0]
+  );
+
+  useEffect(() => {
+    if (!localStorageIsAvailable) return;
+    localStorage.setItem("currentPage", currentPage);
+  }, [currentPage]);
+
   return (
-    <AppContext.Provider value={{ mode, setMode }}>
+    <AppContext.Provider
+      value={{ mode, setMode, pages, setPages, currentPage, setCurrentPage }}
+    >
       <ModeSelector />
+      {mode === "index" && <h1>INDEX PLACEHOLDER</h1>}
+      {mode === "read" && <h1>READ PLACEHOLDER</h1>}
+      {mode === "edit" && <h1>EDIT PLACEHOLDER</h1>}
     </AppContext.Provider>
   );
 };
